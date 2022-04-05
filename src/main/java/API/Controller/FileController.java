3da4fafc40eadddcd22f80d56448bd69a620f0be
vivien <vivien.chouet@gmail.com>
2022-04-05 16:17:47 +0200

    Ajout controller upload files

--- src/main/java/API/Controller/FileController.java
+++ src/main/java/API/Controller/FileController.java
@@ -2,10 +2,20 @@ package API.Controller;
 
 import API.Entity.Entity.Files;
 import API.Service.FileStorageService;
+import API.Utility.ResponseFile;
+import API.Utility.ResponseMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin("http://localhost:4200/")
@@ -18,23 +28,40 @@ public class FileController {
 
 
     @PostMapping("/upload")
-    public ResponseEntity<Files> upload () {
-
-
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<API.Utility.ResponseMessage> upload (@RequestParam ("file")MultipartFile file) {
+        String message = "";
+        try{
+            fileStorageService.store(file);
+            message = "uploaded the file  successfully : " + file.getOriginalFilename();
+            return ResponseEntity.status(HttpStatus.OK).body(new API.Utility.ResponseMessage(message));
+        } catch (IOException e) {
+            message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+        }
     }
 
     @GetMapping("/files")
-    public ResponseEntity<Files> files () {
-
-
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<List<ResponseFile>> files () {
+        List<ResponseFile> files = fileStorageService.getAllFiles().map(dbFile -> {
+            String fileDownloadUri = ServletUriComponentsBuilder
+                    .fromCurrentContextPath()
+                    .path("/files/")
+                    .path(dbFile.getId().toString())
+                    .toUriString();
+            return new ResponseFile(
+                    dbFile.getName(),
+                    fileDownloadUri,
+                    dbFile.getType(),
+                    dbFile.getData().length);
+        }).collect(Collectors.toList());
+        return ResponseEntity.status(HttpStatus.OK).body(files);
     }
 
     @GetMapping("/files/{id}")
-    public ResponseEntity <Files> idFile (@PathVariable int id){
-
-
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity <byte[]> idFile (@PathVariable int id){
+        Files fileDB = fileStorageService.getFile(id);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileDB.getName() + "\"")
+                .body(fileDB.getData());
     }
 }
